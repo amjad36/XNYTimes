ffd800c98d76b3ad07de4fa7c34bfbd679a03d47
Amjad Khan <amjad.khan@conduent.com>
2019-10-28 02:35:25 +0530

    Initial Commit

---
+++ XNYTimes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XNYTimes
+//
+//  Created by Amjad Khan on 28/10/19.
+//  Copyright © 2019 Conduent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
